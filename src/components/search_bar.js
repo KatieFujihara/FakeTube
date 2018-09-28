432118de7041d7cf7496e1a33b27928927e6da39
Kaitlyn <katiefujihara@gmail.com>
2018-09-28 13:16:47 -0700

    start react tutorial

---
+++ src/components/search_bar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// use class component instead of functional component
+class SearchBar extends Component {
+  render() {
+    return <input />;
+  }
+  // eventhandler
+  onInputChange(){
+
+  }
+}
+
+export default SearchBar;
