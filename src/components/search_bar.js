a5f13027911ec412938a38fb8f8977574302233f
Kaitlyn <katiefujihara@gmail.com>
2018-09-28 15:34:14 -0700

    get youtube api call to work

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 
 // use class component instead of functional component
 class SearchBar extends Component {
-  render() {
-    return <input />;
-  }
-  // eventhandler
-  onInputChange(){
+  // only class state components have states, this is how you set it up
+  constructor(props) {
+    super(props);
 
+    this.state = { term: '' };
+  }
+
+  render() {
+  // onChange is a React defined property
+    return (
+      <div>
+        <input
+          value = {this.state.term}
+          onChange={event => this.setState({term: event.target.value})} />
+      </div>
+    );
   }
 }
 
 export default SearchBar;
+
+// state plain javascript object used to record changes in object
--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar'
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list'
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyDvPvEioXKpKXQf48rA0o6H-RNiYIhzywE';
+
 // Create a new component. This component should produce some HTML
 // const is a ES6 concept declaring a constant variable
 // JSX is a subset of JavaScript (looks like HTML but it's actually JavaScript)
 
-const App = () => {
-  return <div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      // this.setState({ videos: videos});
+    });
+  }
+
+  render() {
+  return (
+  <div>
     <SearchBar />
-  </div>;
+    <VideoList videos={this.state.videos} />
+  </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
