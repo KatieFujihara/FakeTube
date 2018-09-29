c8c8de5c92e5f33897dc703190128798f2397717
Kaitlyn <katiefujihara@gmail.com>
2018-09-28 21:52:47 -0700

    get videos to update

--- src/index.js
+++ src/index.js
@@ -19,8 +19,10 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     };
-
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    this.videoSearch('surfboards');
+  }
+    videoSearch(term){
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos : videos,
         selectedVideo: videos[0]
@@ -32,9 +34,10 @@ class App extends Component {
   render() {
   return (
   <div>
-    <SearchBar />
-    <VideoDetail video={this.state.selectedVideo}/>
-    <VideoList videos={this.state.videos} />
+    <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+    <VideoDetail
+    video={this.state.selectedVideo}/>
+    <VideoList onVideoSelect = {selectedVideo => this.setState({selectedVideo}) } videos={this.state.videos} />
   </div>
     );
   }
